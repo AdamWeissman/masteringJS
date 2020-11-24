a4e8457660daae176a558ee112b8f0ef0741dfa7
AdamWeissman <adam@comedytragedyepic.com>
2020-11-24 16:12:52 -0500

    refactored with timeout to make the recursion seem more 'natural'

--- recursion.js
+++ recursion.js
@@ -1,17 +1,23 @@
 let theGuess = document.getElementById("theGuess")
 
 randomGuess = (Math.floor(Math.random() * 10)) + 1
-theGuess.innerText = "?"
+theGuess.innerText = "click me to begin"
 
 theGuess.onclick = guesser
 
-function guesser(e) {
-  e.preventDefault()
+function guesser() {
+  //e.preventDefault()
   userGuess = prompt("Guess!")
   if (userGuess > randomGuess) {
     theGuess.innerText = "LOWER!"
+    setTimeout(function() {
+      guesser()
+    }, 2000)
   } else if (userGuess < randomGuess) {
     theGuess.innerText = "HIGHER!"
+    setTimeout(function() {
+      guesser()
+    }, 2000)
   } else if (userGuess == randomGuess) {
     theGuess.innerText = randomGuess + ", you got it!"
   }
