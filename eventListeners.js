85f95cbfbffac4233915b071b17911f0930a45d0
AdamWeissman <adam@comedytragedyepic.com>
2020-11-29 15:52:36 -0500

    event listener options... should be useful for building a chrome extension

---
+++ eventListeners.js
@@ -0,0 +1,12 @@
+/*
+
+thing.addEventListener("click", function(){
+  doSomeStuffHere
+})
+
+thing.addEventListener("click", someNamedFunction)
+
+
+if you do console.dir(thing) it will show you many things you can do
+
+*/
