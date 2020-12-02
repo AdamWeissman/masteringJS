40de5a8392689ee5eafff364bcf3693cdd83a442
AdamWeissman <adam@comedytragedyepic.com>
2020-12-02 15:21:31 -0500

    oops, section 7 palindrome question

--- dsalgo/linkedList.js
+++
@@ -1,4 +0,0 @@
-// to run this file from main directory...
-// node ./dsalgo/linkedlist.js
-
-console.log("Hello from LinkedList")
---
+++ dsalgo/s7palindrome.js
@@ -0,0 +1,4 @@
+// to run this file from main directory...
+// node ./dsalgo/s7palindrome.js
+
+console.log("RUNNING PALINDROME")
