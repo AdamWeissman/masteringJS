6016cb7285a9f660f091bf8f95a9b7f072418c13
AdamWeissman <adam@comedytragedyepic.com>
2020-12-02 15:25:00 -0500

    some problem solving tips in the notes

--- dsalgo/s7palindrome.js
+++ dsalgo/s7palindrome.js
@@ -2,3 +2,14 @@
 // node ./dsalgo/s7palindrome.js
 
 console.log("RUNNING PALINDROME")
+
+/*
+
+  Main Problem in Context of Input:
+    -Edge Cases
+  Sub Problems in Terms of Intermediary Steps:
+    -Things to Keep Track Of
+    -Inputs and Outputs
+  Final Output:
+
+*/
