76e2870365fc26f8ed111fb2a34d9897bcae2e53
AdamWeissman <adam@comedytragedyepic.com>
2020-12-05 13:42:03 -0500

    sorta simple sort

--- dsalgo/anagramPractice.js
+++ dsalgo/anagramPractice.js
@@ -3,8 +3,10 @@ const re = /[^\w]/g
 function anagrams(stringA, stringB) {
   const w1 = stringA.replace(re, "").toLowerCase()
   const w2 = stringB.replace(re, "").toLowerCase()
-  console.log(w1, w2)
+  const x = w1.split('').sort().join('')
+  const y = w2.split('').sort().join('')
+  return (x == y)
 }
 console.log("HELLO FROM ANAGRAMS")
 
-anagrams("cat !!", "tA ? ?C")
+console.log(anagrams("cat !!", "tA ? ?C"))
