b75285c4af64ef4e6d993ed6102a83829cc979de
AdamWeissman <adam@comedytragedyepic.com>
2020-12-05 20:23:05 -0500

    higher order function

--- advancedJSstuff/higherOrderFunctions.js
+++ advancedJSstuff/higherOrderFunctions.js
@@ -1,3 +1,13 @@
-function letAdamLogin(){
-  let array []
-}
+// function letAdamLogin() {
+//   let array = []
+//   for (let i = 0; i < 1000000; i++) {
+//     array.push(i)
+//   }
+//   return 'Access Granted!'
+// }
+
+// console.log(letAdamLogin())
+
+//console.log("hello from higher order functions")
+
+// nothing special about this lesson so far... just function calling another function
