c1489e423fd07c77f5243c63804ef65a06df8712
AdamWeissman <adam@comedytragedyepic.com>
2020-12-07 13:41:43 -0500

    testing my new approach to algo and ds

--- dsalgo/steps/index.js
+++ dsalgo/steps/index.js
@@ -17,18 +17,34 @@
 //       '### '
 //       '####'
 
-function steps(n, row = 0, stair = '') {
-  if (n === row) {
-    return;
-  }
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
 
-  if (n === stair.length) {
-    console.log(stair);
-    return steps(n, row + 1);
-  }
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
 
-  const add = stair.length <= row ? '#' : ' ';
-  steps(n, row, stair + add);
+//   const add = stair.length <= row ? '#' : ' ';
+//   steps(n, row, stair + add);
+// }
+
+
+function steps(n) {
+    numOfSpaces = 1
+    totalSteps = new Array(n).fill('#')
+    for (let level = 1; level < totalSteps.length; level++) {
+      levelArray = [].concat(totalSteps[level - 1])
+      console.log(levelArray)  
+    }
+  
+
+
 }
+
+
+
 
 module.exports = steps;
