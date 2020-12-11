a7fe0859b4589c7ee41e6b867826ea4c83f58b76
AdamWeissman <adam@comedytragedyepic.com>
2020-12-10 22:21:58 -0500

    testing inheritance

--- oop/classBased.js
+++ oop/classBased.js
@@ -1,18 +1,18 @@
 //ES6 CLASS
 
-class Elf {
-  constructor(name, weapon) {
-    this.name = name;
-    this.weapon = weapon;
-  }
-  attack() { // behind the scenes this function is basically inherited by whatever is made from the constructor
-    return 'attack with ' + this.weapon
-  }
-}
+// class Elf {
+//   constructor(name, weapon) {
+//     this.name = name;
+//     this.weapon = weapon;
+//   }
+//   attack() { // behind the scenes this function is basically inherited by whatever is made from the constructor
+//     return 'attack with ' + this.weapon
+//   }
+// }
 
-const peter = new Elf('Peter', 'stones')
-console.log(peter.attack());
-console.log(peter.name)
+// const peter = new Elf('Peter', 'stones')
+// console.log(peter.attack());
+// console.log(peter.name)
 
 
 // CAN USE Object.create() to accomplish the same as above
@@ -70,3 +70,48 @@ FOUR WAYS TO USE THIS
   person4.hi() THIS refers to the lexical scope on line 64 with console.log... 
   if not an arrow function it would refer to the larger scope
 */
+
+class Character {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+
+  attack() {
+    return 'attack with ' + this.weapon;
+  }
+}
+
+
+class Elf extends Character {
+  constructor(name, weapon, type) {
+    super(name, weapon) // this refers to the super class, and allows type to be added to the super class
+    console.log("first this", this)
+    this.type = type
+    console.log("new this", this)
+  }
+}
+
+class Ogre extends Character {
+  constructor(name, weapon, color) {
+    super(name, weapon);
+    this.color = color;
+  }
+  makeFort() {
+    return 'strongest fort in the world made';
+  }
+}
+
+
+
+
+const dolby = new Elf('Dolby', 'ninja stars', 'house elf')
+
+const shrek = new Ogre('Shrek', 'club', 'green')
+
+console.log(dolby)
+console.log(shrek)
+console.log(shrek.makeFort())
+console.log(Ogre.prototype.isPrototypeOf(shrek))
+console.log(dolby instanceof Character)
+console.log(dolby instanceof Elf)
