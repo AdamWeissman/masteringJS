0c01bf179cfad18e13fa08539262fa6db385d474
AdamWeissman <adam@comedytragedyepic.com>
2020-12-10 21:52:16 -0500

    4 types of this

--- oop/classBased.js
+++ oop/classBased.js
@@ -5,7 +5,7 @@ class Elf {
     this.name = name;
     this.weapon = weapon;
   }
-  attack() {
+  attack() { // behind the scenes this function is basically inherited by whatever is made from the constructor
     return 'attack with ' + this.weapon
   }
 }
@@ -13,3 +13,60 @@ class Elf {
 const peter = new Elf('Peter', 'stones')
 console.log(peter.attack());
 console.log(peter.name)
+
+
+// CAN USE Object.create() to accomplish the same as above
+
+
+/*
+FOUR WAYS TO USE THIS
+  ...new binding this...
+
+  function Person(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+
+  const person1 = new Person('Xavier', 55)
+  person1 is "this"
+
+  ...implicit binding this...
+  const person = {
+    name: 'Karen',
+    age: 40,
+    hi() {
+      console.log('hi' + this.name)
+    }
+  }
+
+  this refers to const person above
+
+  ...explicit binding...
+
+  const person3 = {
+    name: 'Karen',
+    age: 40, 
+    hi: function() {
+      console.log('hi' + this.setTimeout)
+    }.bind(window)
+  }
+
+  person3.hi()  THIS refers to the window
+
+
+  ...arrow function...
+
+  const person4 = {
+    name: 'Karen',
+    age: 40, 
+    hi: function() {
+      var inner = () => {
+        console.log('hi ' + this.name)
+      }
+      return inner()
+    }
+  }
+
+  person4.hi() THIS refers to the lexical scope on line 64 with console.log... 
+  if not an arrow function it would refer to the larger scope
+*/
