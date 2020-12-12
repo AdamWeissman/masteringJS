2c25e9f6cde94a8bb60792568f3bbdb67d549044
AdamWeissman <adam@comedytragedyepic.com>
2020-12-11 20:51:18 -0500

    pure functions

--- functionalP/functionalP.js
+++ functionalP/functionalP.js
@@ -1,3 +1,28 @@
 // no side effects
 // input -> output 
 
+
+const array = [1,2,3]
+
+function removeLastItem(arr) {
+  const newArray = [].concat(arr); // creates copy of the array as opposed to mutating it
+  newArray.pop() 
+  return newArray
+}
+
+console.log(removeLastItem(array))
+
+// referential transparency
+
+function a(num1, num2) {
+  return num1 + num2
+}
+
+function b(num) {
+  return num * 2
+}
+
+
+// referential transparency in action below
+console.log(b(a(3, 4)))
+console.log(b(7))
