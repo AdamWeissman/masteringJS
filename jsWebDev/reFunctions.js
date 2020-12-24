a26f32fdd75d81f430fe3408a15dda5706a031f0
AdamWeissman <adam@comedytragedyepic.com>
2020-12-24 14:15:41 -0500

    functions

--- jsWebDev/reFunctions.js
+++ jsWebDev/reFunctions.js
@@ -7,3 +7,22 @@ function collectEggs() {
 console.log(totalEggs);
 collectEggs()
 console.log(totalEggs);
+
+let bird = "Scarlet Macaw"
+function theBird() {
+  let bird = "Blue Heron" 
+  console.log(bird)
+}
+
+theBird()
+console.log(bird)
+
+// COMPARE BELOW ... functionally equivalent... but keep in mind hoisting.
+
+function add(x, y) {
+  return x + y
+}
+
+const add = function (x, y) {
+  return x + y
+}
