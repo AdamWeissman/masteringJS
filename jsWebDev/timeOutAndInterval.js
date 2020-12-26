417e81aa02c5878aa6cfa05df56c640bf271bb2c
AdamWeissman <adam@comedytragedyepic.com>
2020-12-26 16:36:39 -0500

    interval and timeout

---
+++ jsWebDev/timeOutAndInterval.js
@@ -0,0 +1,11 @@
+// console.log("yo!")
+
+// setTimeout(() => {
+//   console.log("you there?")
+// }, 3000)
+
+// const id = setInterval(() => {
+//   console.log(Math.random())
+// }, 2000);
+
+// clearInterval(id)
